650c05548e8c596699ee497efbb1b9e7ea9dd253
wovert <wovert@126.com>
2019-09-26 06:58:43 +0800

    iOS初始化项目开发

--- HelloiOS/HelloiOS/ViewController.h
+++ HelloiOS/HelloiOS/ViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
+// IBOutlet 输出口
+//@property (nonatomic, strong) IBOutlet UILabel *label;
 @end
 
--- HelloiOS/HelloiOS/ViewController.m
+++ HelloiOS/HelloiOS/ViewController.m
@@ -8,12 +8,20 @@
 
 #import "ViewController.h"
 
+#pragma mark 类扩展:可充属性和方法声明
+// ()类扩展（扩展属性和方法）
+// (HightClass) 分类（只能扩展方法）
+// 可以方法.h文件中
+// 作用：存放类的私有属性
 @interface ViewController ()
-
+@property (nonatomic, strong) IBOutlet UILabel *label;
+-(void)run;
 @end
 
 @implementation ViewController
-
+-(void)run {
+    NSLog(@"run method...");
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -21,15 +29,25 @@
 
 #pragma mark 监听红色按钮点击
 -(IBAction)redClick {
+    // 获取文本标签
+    self.label.textColor = [UIColor redColor];
+    self.label.text = @"I'm red color";
+    self.label.font = [UIFont systemFontOfSize: 20];
     NSLog(@"redClick>>>");
 }
 
 #pragma mark 监听绿色按钮点击
 -(IBAction)greenClick {
+    self.label.textColor = [UIColor greenColor];
+    self.label.text = @"I'm green color";
+    self.label.font = [UIFont systemFontOfSize:30];
     NSLog(@"greenClick>>>");
 }
 #pragma mark 监听蓝色按钮点击
 -(IBAction)blueClick {
+    self.label.textColor = [UIColor blueColor];
+    self.label.text = @"I'm blue color";
+    self.label.font = [UIFont systemFontOfSize: 40];
     NSLog(@"blueClick>>>");
 }
 
